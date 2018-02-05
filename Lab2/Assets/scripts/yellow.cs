4a1f751d0753c6c5f8725beb2aaf1117b126b30a
johnalanC <johnalan.calma@gmail.com>
2018-02-04 23:54:36 -0500

    added Lab2

---
+++ Lab2/Assets/scripts/yellow.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yellow : MonoBehaviour {
+
+    Vector2 originalPos = new Vector2(0.38f, 1.97f);
+
+
+    Vector2 shrek_originalPos;
+    Vector2 shrek_yellow_originalPos;
+    Vector2 shrek_red_originalPos;
+    Vector2 shrek_blue_originalPos;
+
+    public GameObject shrek;
+    public GameObject shrek_yellow;
+    public GameObject shrek_red;
+    public GameObject shrek_blue;
+
+	// Use this for initialization
+	void Start () {
+        shrek.GetComponent<Rigidbody2D>().gravityScale = 0;
+        shrek_yellow.GetComponent<Rigidbody2D>().gravityScale = 0;
+        shrek_red.GetComponent<Rigidbody2D>().gravityScale = 0;
+        shrek_blue.GetComponent<Rigidbody2D>().gravityScale = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+ 
+    private void OnMouseUp()
+    {
+        shrek.transform.position = originalPos;
+        shrek_yellow.transform.position = originalPos;
+        shrek_red.transform.position = originalPos;
+        shrek_blue.transform.position = originalPos;
+
+        shrek.GetComponent<Rigidbody2D>().gravityScale = 0;
+        shrek_yellow.GetComponent<Rigidbody2D>().gravityScale = 0;
+        shrek_red.GetComponent<Rigidbody2D>().gravityScale = 0;
+        shrek_blue.GetComponent<Rigidbody2D>().gravityScale = 0;
+
+
+
+
+        shrek.GetComponent<Rigidbody2D>().gravityScale = 10;
+        shrek_yellow.GetComponent<Rigidbody2D>().gravityScale = 0;
+        shrek_red.GetComponent<Rigidbody2D>().gravityScale = 10;
+        shrek_blue.GetComponent<Rigidbody2D>().gravityScale = 10;
+                    
+    }
+}
